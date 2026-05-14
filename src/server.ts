83bc2c791e4232deead4a41a8eb232058495ca89
hayder <hayderbd4290@gmail.com>
2026-05-14 23:24:18 +0600

    make connection string secure by dotenv

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,16 @@ import express, {
   type Response,
 } from "express";
 import { Pool } from "pg";
+import config from "./config/env.config";
 const app: Application = express();
-const PORT = 5000;
+const PORT = config.port;
 
 // middleware
 app.use(express.json());
 
 // connected neon db
 const pool = new Pool({
-  connectionString:
-    "postgresql://neondb_owner:npg_lJ2aGNLsv5qF@ep-summer-mud-ap9m1juz-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: config.connectionString,
 });
 
 // create user Table
